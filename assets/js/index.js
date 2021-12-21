9b837e33850a3a41299185f92f1881e1788c78f7
exdeejay <13634296+exdeejay@users.noreply.github.com>
2021-12-21 15:02:53 -0600

    responsive update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,19 @@
+/**
+ * @param {*} elem 
+ * @returns {string[]}
+ */
+function getClasses(elem) {
+	return elem.getAttribute('class').split(' ');
+}
+
+/**
+ * @param {*} elem 
+ * @param {string[]} classes 
+ */
+function setClasses(elem, classes) {
+	elem.setAttribute('class', classes.join(' '))
+}
+
 async function sleep(secs) {
     return new Promise((resolve) => {
         setTimeout(resolve, secs * 1000);
@@ -23,17 +39,19 @@ document.body.onload = async () => {
     let regex = document.querySelector('header h1 span.regex');
 	let subtitle = document.querySelector('header h2');
 	await sleep(2);
+	await animateType(regex, '.*', 0.5);
+	await sleep(1);
 	while (true) {
-		await animateType(regex, '.*', 0.5);
-		await sleep(1);
-		await animateBackspace(regex, 0.1);
+		await animateBackspace(regex, 0.25);
 		await sleep(0.5);
 		await animateType(regex, 'dotWildcard', 0.1);
-		if (subtitle.getAttribute('class') == 'subtitle-hidden') {
-			subtitle.removeAttribute('class');
+		if (getClasses(subtitle).includes('subtitle-hidden')) {
+			setClasses(subtitle, getClasses(subtitle).filter(i => i != 'subtitle-hidden'));
 		}
 		await sleep(10);
 		await animateBackspace(regex, 0.1);
 		await sleep(1);
+		await animateType(regex, '.*', 0.5);
+		await sleep(5);
 	}
 };
