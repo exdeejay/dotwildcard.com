4be314012f38df149d785fa56e4bcc2c8f1e8fba
exdeejay <13634296+exdeejay@users.noreply.github.com>
2023-02-25 00:50:59 -0500

    change date style

--- next/src/pages/posts.tsx
+++ next/src/pages/posts.tsx
@@ -16,7 +16,13 @@ export default function Posts({ posts }: PostsProps) {
                 <Link key={post.slug} href={`/posts/${post.slug}`}>
                     <li className="bg-primary-800 p-8 my-6">
                         <h2 className="text-2xl text-primary-300 font-bold font-mono tracking-tight">{post.title}</h2>
-                        <h3 className="text-teal-700 font-mono mb-2 tracking-tight">{post.publish}</h3>
+                        {post.publish && 
+                            <h3 className="text-teal-700 font-mono mb-2 tracking-tight">{
+                                new Date(post.publish).toLocaleDateString(undefined, {
+                                    dateStyle: 'medium'
+                                })
+                            }</h3>
+                        }
                         <p className="text-zinc-200">{post.description}</p>
                     </li>
                 </Link>
